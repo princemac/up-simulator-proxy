6e4f4744ea06a6132c695a9e0be22edb1394d678
Neelam Kushwah <neelam.kushwah@gm.com>
2024-03-06 09:51:14 -0500

    Add copyright header to all the files

--- app/src/main/java/org/eclipse/uprotocol/simulatorproxy/BaseService.java
+++ app/src/main/java/org/eclipse/uprotocol/simulatorproxy/BaseService.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2024 General Motors GTO LLC
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * SPDX-FileType: SOURCE
+ * SPDX-FileCopyrightText: 2024 General Motors GTO LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package org.eclipse.uprotocol.simulatorproxy;
 
 import static org.eclipse.uprotocol.common.util.UStatusUtils.isOk;
@@ -94,11 +117,10 @@ public class BaseService extends Service {
     }
 
     public UStatus registerMethod(@NonNull UUri methodUri) {
-//        return CompletableFuture.supplyAsync(() -> {
             final UStatus status = mUPClient.registerRpcListener(methodUri, mURpcListener);
 
             return logStatus("registerMethod", status, Key.URI, stringify(methodUri));
-//        });
+
     }
 
     public UStatus publish(@NonNull UMessage message) {
